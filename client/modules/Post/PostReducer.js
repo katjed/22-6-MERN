110b29ba5b8f6b81e34e66aa560f5e478fecfc9b
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-10 01:09:08 +0200

    Add edit

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,28 +1,33 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, ADD_POSTS, DELETE_POST, EDIT_POST } from './PostActions';
 
 // Initial State
 const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST :
-      return {
-        data: [action.post, ...state.data],
-      };
+	switch (action.type) {
+		case ADD_POST :
+			return {
+				data: [action.post, ...state.data],
+			};
+			
+		case ADD_POSTS :
+			return {
+				data: action.posts,
+			};
 
-    case ADD_POSTS :
-      return {
-        data: action.posts,
-      };
+		case DELETE_POST :
+			return {
+				data: state.data.filter(post => post.cuid !== action.cuid),
+			};
 
-    case DELETE_POST :
-      return {
-        data: state.data.filter(post => post.cuid !== action.cuid),
-      };
+		case EDIT_POST :
+  			return {
+    			data: state.data.map(post => { return post.cuid === action.cuid ? Object.assign({}, post, action.post) : post } ),
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
 /* Selectors */
