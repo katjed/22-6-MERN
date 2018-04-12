56a9995aa84ac8e97322d686e01365f041c949bc
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-13 01:45:38 +0200

    Update

--- client/modules/About/About.js
+++ client/modules/About/About.js
@@ -3,14 +3,13 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Import Style
-import styles from './About.css';
+//import styles from './About.css';
 
 class About extends Component {
   	render() {
 		return (
-			<div className={styles.about}>
-				<h2>About</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dolor vel nulla mollis faucibus hendrerit non augue.</p>
+			<div>
+				<h2>About</h2>        
 			</div>
 		);
   }
